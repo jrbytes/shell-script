f7fe03f1c689140f6aea536b1867044d29d2b744
Jr Bytes <itamazor@gmail.com>
2020-09-28 09:38:23 -0300

    Added functions.

---
+++ 13-functions/functionsCheck.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+function funcCheck()
+{
+  returningValue="I love Windows"
+}
+
+returningValue="I love Linux"
+echo $returningValue
+
+funcCheck
+echo $returningValue
---
+++ 13-functions/functionsParams.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+function funcPrint()
+{
+  echo $1 $2 $3 $4
+}
+
+funcPrint Hi this is linux

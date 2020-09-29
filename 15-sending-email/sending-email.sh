2694a4a3a6e8bce12a148e92847a28950fbf08be
Jr Bytes <itamazor@gmail.com>
2020-09-28 21:00:40 -0300

    Added sengnid emails.

---
+++ 15-sending-email/sending-email.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# sudo apt install ssmtp
+ssmtp jrbytesbackup@gmail.com
+
+# To: jrbytesbackup@gmail.com
+# From: jrbytesbackup@gmail.com
+# Cc: jrbytesbackup@gmail.com
+# Subject: test
+# This is body
